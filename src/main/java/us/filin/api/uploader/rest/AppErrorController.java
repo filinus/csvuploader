846f8663977d3d288444a0f569feb4fd73cbff93
Val Filin <val@filin.us>
2018-12-02 23:08:56 -0800

    return errors as JSON

---
+++ src/main/java/us/filin/api/uploader/rest/AppErrorController.java
@@ -0,0 +1,32 @@
+package us.filin.api.uploader.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+@Slf4j
+public class AppErrorController extends AbstractErrorController {
+    public AppErrorController(ErrorAttributes errorAttributes) {
+        super(errorAttributes);
+    }
+
+    @RequestMapping(value = "/error", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map<String, Object> handleError(HttpServletRequest request) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(request, false);
+        return errorAttributes;
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
